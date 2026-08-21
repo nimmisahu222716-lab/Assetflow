b5cefe7aeea5e551004fb6b3666be457b05e67ce
nimmisahu222716-lab <nimmisahu222716@gmail.com>
2026-08-22 01:29:14 +0530

    fix: ensure CORS headers are set prior to express handler invocation in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
-const app = require('../server/app');
-
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
 
-  return app(req, res);
+  try {
+    const app = require('../server/app');
+    return app(req, res);
+  } catch (err) {
+    console.error('Vercel Serverless Handler Error:', err);
+    return res.status(500).json({
+      error: 'Internal Serverless Handler Error',
+      message: err.message
+    });
+  }
 };
