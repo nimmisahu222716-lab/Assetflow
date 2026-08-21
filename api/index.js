dc347f124675bedd5a985c4c0f69514c36e023cf
nimmisahu222716-lab <nimmisahu222716@gmail.com>
2026-08-22 01:22:24 +0530

    fix: resolve CORS preflight specification conflict and add Vercel serverless OPTIONS handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
 const app = require('../server/app');
 
-module.exports = app;
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return app(req, res);
+};
